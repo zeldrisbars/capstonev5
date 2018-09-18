dcb5b94c7a96f2a455a379cd38181d3d7b71cdba
zeldrisbars <zeldrisbars@users.noreply.github.com>
2018-09-18 08:54:48 +0800

    Rev

--- app/Http/Controllers/CreateBlotterController.php
+++ app/Http/Controllers/CreateBlotterController.php
@@ -28,7 +28,7 @@ protected function validator(array $request)
 public function index()
     {
         $blotter = CreateBlotter::where('id',1)->get();
-        $id = CreateBlotter::get('id');
+        $id = CreateBlotter::where('id')->get();
         $createblotter = CreateBlotter::all();
         return view('blotter.createblotter',compact('createblotter','blotter','id'));
     }    
