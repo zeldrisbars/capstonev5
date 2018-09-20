71b4cda96c7957f3c283ec95158e36b3bb1609f5
zeldrisbars <zeldrisbars@users.noreply.github.com>
2018-09-20 08:34:20 +0800

    Rev

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -178,14 +178,14 @@ $('#deleteModal').on('show.bs.modal', function (event) {
     console.log('Modal Opened')
   var button = $(event.relatedTarget) // Button that triggered the modal
   
-  var e_id = button.data('edit_id')
+  var id = button.data('myid')
 
   // Extract info from data-* attributes
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
   var modal = $(this)
  
-  modal.find('.modal-body #e_id').val(e_id)
+  modal.find('.modal-body #id').val(id)
 })
 
 </script>
